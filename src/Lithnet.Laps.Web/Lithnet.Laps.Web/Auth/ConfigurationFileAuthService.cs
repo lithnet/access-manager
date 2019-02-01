0d514233159307652acfb71a8512b734cd252adf
Johan Vervloet <johan.vervloet@kuleuven.be>
2019-02-01 21:47:29 +0100

    WIP: AuthService.
    This will be a messy interface anyway.

--- src/Lithnet.Laps.Web/Lithnet.Laps.Web/Auth/ConfigurationFileAuthService.cs
+++ src/Lithnet.Laps.Web/Lithnet.Laps.Web/Auth/ConfigurationFileAuthService.cs
@@ -7,7 +7,19 @@ namespace Lithnet.Laps.Web.Auth
 {
     public class ConfigurationFileAuthService : IAuthService
     {
-        public bool CanAccessPassword(string userName, string computerName)
+        /// <summary>
+        /// Check whether the user with name <paramref name="userName"/> can 
+        /// access the password of the computer with name
+        /// <paramref name="computerName"/>
+        /// </summary>
+        /// <param name="userName">name of the user</param>
+        /// <param name="computerName">name of the computer</param>
+        /// <param name="target">Target section in the web.config-file.
+        /// FIXME: This shouldn't be in this interface. But I can't leave it out
+        /// yet, because the code figuring out the target has way too many dependencies.
+        /// </param>
+        /// <returns><c>true</c> or <c>false</c> accordingly</returns>
+        public bool CanAccessPassword(string userName, string computerName, TargetElement target = null)
         {
             throw new NotImplementedException();
         }
