857f292431b5bf03fc1e5dd9891f6528455235e9
Johan Vervloet <johan.vervloet@kuleuven.be>
2019-02-01 15:19:48 +0100

    Added some dependency injection.

---
+++ src/Lithnet.Laps.Web/Lithnet.Laps.Auth/ConfigurationFileAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lithnet.Laps.Auth
+{
+    public class ConfigurationFileAuthService : IAuthService
+    {
+        public bool CanAccessPassword(string userName, string computerName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
