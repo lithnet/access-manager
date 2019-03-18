7758ef92c2fdf72eab4cc86124e07a8d7734d18d
Johan Vervloet <johan.vervloet@gmail.com>
2019-03-18 13:07:35 +0100

    Value object UsersToBeNotified, #1.

---
+++ src/Lithnet.Laps.Web/Lithnet.Laps.Web/Audit/UsersToBeNotified.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+
+namespace Lithnet.Laps.Web.Audit
+{
+    public class UsersToBeNotified
+    {
+        public IImmutableSet<string> OnSuccess { get; private set; }
+        public IImmutableSet<string> OnFailure { get; private set; }
+
+        public UsersToBeNotified(IImmutableSet<string> onSuccess, IImmutableSet<string> onFailure)
+        {
+            OnSuccess = onSuccess;
+            OnFailure = onFailure;
+        }
+
+        public UsersToBeNotified NotifyOnSuccess(IImmutableSet<string> emails)
+        {
+            return new UsersToBeNotified(OnSuccess.Union(emails), OnFailure);
+        }
+
+        public UsersToBeNotified NotifyOnFailure(IImmutableSet<string> emails)
+        {
+            return new UsersToBeNotified(OnSuccess, OnFailure.Union(emails));
+        }
+    }
+}
