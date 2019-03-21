a1d4ac3a5a16aab7b50acf8a8ab3e85e4829a3f8
Johan Vervloet <johan.vervloet@gmail.com>
2019-03-21 18:01:20 +0100

    Some extra assertions, #1.

--- src/Lithnet.Laps.Web/Lithnet.Laps.WebTests/Security/Authorization/ConfigurationFileAuthorizationServiceTests.cs
+++ src/Lithnet.Laps.Web/Lithnet.Laps.WebTests/Security/Authorization/ConfigurationFileAuthorizationServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Lithnet.Laps.Web;
+﻿using System;
+using Lithnet.Laps.Web;
 using Lithnet.Laps.Web.Models;
 using Lithnet.Laps.Web.Security.Authorization.ConfigurationFile;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -11,15 +12,21 @@ namespace Lithnet.Laps.WebTests.Security.Authorization
     public class ConfigurationFileAuthorizationServiceTests
     {
         private Mock<ILogger> dummyLogger;
-        private Mock<ITarget> dummyTarget;
+        private Mock<ITarget> targetStub;
         private Mock<IComputer> dummyComputer;
 
         [TestInitialize]
         public void TestInitialize()
         {
             dummyLogger = new Mock<ILogger>();
-            dummyTarget = new Mock<ITarget>();
+            targetStub = new Mock<ITarget>();
             dummyComputer = new Mock<IComputer>();
+
+            // When ExpireAfter is left out in the configuration file, I have the impression
+            // that this is passed as an empty string. So let's emulate this.
+            targetStub
+                .Setup(t => t.ExpireAfter)
+                .Returns(String.Empty);
         }
 
         [TestMethod()]
@@ -28,7 +35,7 @@ namespace Lithnet.Laps.WebTests.Security.Authorization
             // I admit this is a bad test. It depends too much on the actual implementation.
             // An integration test would be better, but then I would have to know a user
             // from the domain of the developer.
-
+            
             var readerStub = new Mock<IReaderElement>();
             var userStub = new Mock<IUser>();
             var directoryStub = new Mock<IDirectory>();
@@ -37,7 +44,7 @@ namespace Lithnet.Laps.WebTests.Security.Authorization
             readerStub
                 .Setup(r => r.Principal)
                 .Returns("DOMAIN\\username");
-
+            
             directoryStub
                 .Setup(d => d.GetUser(It.IsAny<string>()))
                 .Returns(userStub.Object);
@@ -47,8 +54,8 @@ namespace Lithnet.Laps.WebTests.Security.Authorization
                 .Returns("some-distinguished-name");
 
             availableReadersStub
-                .Setup(ar => ar.GetReadersForTarget(dummyTarget.Object))
-                .Returns(new [] {readerStub.Object});
+                .Setup(ar => ar.GetReadersForTarget(targetStub.Object))
+                .Returns(new [] {readerStub.Object}).Verifiable();
 
             var service = new ConfigurationFileAuthorizationService(
                 dummyLogger.Object,
@@ -56,9 +63,14 @@ namespace Lithnet.Laps.WebTests.Security.Authorization
                 availableReadersStub.Object
             );
 
-            Assert.IsTrue(
-                service.CanAccessPassword(userStub.Object, dummyComputer.Object, dummyTarget.Object).IsAuthorized
-            );
+            var authorizationResponse =
+                service.CanAccessPassword(userStub.Object, dummyComputer.Object, targetStub.Object);
+            
+            availableReadersStub.Verify();
+            
+            Assert.IsTrue(authorizationResponse.IsAuthorized);
+            // The reader principal should be in extra info.
+            Assert.AreEqual("DOMAIN\\username", authorizationResponse.ExtraInfo);
         }
     }
 }
