0611a3513c1cd270f88ec33220e7fa27a9fd0737
Ryan Newington <ryan@lithiumblue.com>
2020-08-04 09:21:37 +1000

    Access manager rename updates

--- src/Lithnet.AccessManager/Lithnet.AccessManager.Server.UI/Constants.cs
+++ src/Lithnet.AccessManager/Lithnet.AccessManager.Server.UI/Constants.cs
@@ -7,7 +7,7 @@ namespace Lithnet.AccessManager.Server.UI
 {
     public static class Constants
     {
-        public const string LinkDownloadAccessManagerAgent = "https://github.com/lithnet/laps-web/releases";
+        public const string LinkDownloadAccessManagerAgent = "https://github.com/lithnet/access-manager/releases";
         public const string LinkDownloadMsLaps = "https://aka.ms/LAPS";
         public const string UrlProductVersionInfo = "https://lithnet.github.io/access-manager/version.json";
         public const string EventSourceName = "Lithnet Access Manager Configuration Tool";
