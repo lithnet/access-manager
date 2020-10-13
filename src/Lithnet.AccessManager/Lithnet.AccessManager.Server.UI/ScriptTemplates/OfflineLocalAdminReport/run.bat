0267c513c26acacfcaaf10c4e65e4af6884e0e15
Ryan Newington <ryan@lithiumblue.com>
2020-10-14 09:58:46 +1100

    Updates merge script with better error handling Updates license files

--- src/Lithnet.AccessManager/Lithnet.AccessManager.Server.UI/ScriptTemplates/OfflineLocalAdminReport/run.bat
+++ src/Lithnet.AccessManager/Lithnet.AccessManager.Server.UI/ScriptTemplates/OfflineLocalAdminReport/run.bat
@@ -2,3 +2,5 @@
 copy /Y "%~dp0\Get-LocalAdmins.ps1" "%temp%\Get-LocalAdmins.ps1"
 
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%temp%\Get-LocalAdmins.ps1" "%~dp0\.."
+
+del /Q "%temp%\Get-LocalAdmins.ps1"
