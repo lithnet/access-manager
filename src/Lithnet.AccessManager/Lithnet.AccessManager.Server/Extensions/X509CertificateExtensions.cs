fd138b2a6f5d20fd64f07446818df54ce5df878d
Ryan Newington <ryan@lithiumblue.com>
2021-07-08 19:05:16 +1000

    Fixes an issue where an ECDH SSL certificate could not be selected from the UI

--- src/Lithnet.AccessManager/Lithnet.AccessManager.Server/Extensions/X509CertificateExtensions.cs
+++ src/Lithnet.AccessManager/Lithnet.AccessManager.Server/Extensions/X509CertificateExtensions.cs
@@ -50,10 +50,10 @@ namespace Lithnet.AccessManager.Server
 
         public static string GetPrivateKeyPath(X509Certificate2 cert)
         {
-            RSACng cng = cert.PrivateKey as RSACng;
-            RSACryptoServiceProvider crypto = cert.PrivateKey as RSACryptoServiceProvider;
+            CngKey cng = GetPrivateKey(cert);
+            RSACryptoServiceProvider crypto = GetLegacyPrivateKey(cert);
 
-            string name = cng?.Key?.UniqueName ?? crypto?.CspKeyContainerInfo?.UniqueKeyContainerName;
+            string name = cng?.UniqueName ?? crypto?.CspKeyContainerInfo?.UniqueKeyContainerName;
 
             if (string.IsNullOrWhiteSpace(name))
             {
@@ -72,6 +72,41 @@ namespace Lithnet.AccessManager.Server
             if (File.Exists(path))
             {
                 return path;
+            }
+
+            return null;
+        }
+
+        public static string Sha256Thumbprint(this X509Certificate2 cert)
+        {
+            return cert.GetCertHashString(HashAlgorithmName.SHA256);
+        }
+
+        private static RSACryptoServiceProvider GetLegacyPrivateKey(X509Certificate2 cert)
+        {
+            try
+            {
+                return cert.PrivateKey as RSACryptoServiceProvider;
+            }
+            catch
+            {
+                //ignore
+            }
+
+            return null;
+        }
+
+        public static CngKey GetPrivateKey(X509Certificate2 cert)
+        {
+            if (cert.PublicKey.Oid.Value == "1.2.840.10045.2.1")
+            {
+                return ((ECDsaCng)cert.GetECDsaPrivateKey()).Key;
+            }
+
+            if (cert.PublicKey.Oid.Value == "1.2.840.113549.1.1.1")
+
+            {
+                return ((RSACng)cert.GetRSAPrivateKey()).Key;
             }
 
             return null;
