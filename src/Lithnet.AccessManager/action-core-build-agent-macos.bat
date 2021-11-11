57a0ee7eaa0f61604fdac3da97813cb65f355da8
Ryan Newington <ryan@lithiumblue.com>
2021-11-12 10:30:55 +1100

    Fixes issue with macOS package

--- src/Lithnet.AccessManager/action-core-build-agent-macos.bat
+++ src/Lithnet.AccessManager/action-core-build-agent-macos.bat
@@ -3,9 +3,11 @@ SETLOCAL
 SET agentProjectMacOs=%solutiondir%\Lithnet.AccessManager.Agent.MacOs\Lithnet.AccessManager.Agent.MacOs.csproj
 SET outputdirx64MacOs=%amsBuildFolder%\macos-x64
 SET outputdirarm64MacOs=%amsBuildFolder%\macos-arm64
-SET pkgStagingPath=~/Documents/LithnetAccessManagerAgent-%version%.macos-x64.pkg
+SET pkgStagingPathx64=~/Documents/LithnetAccessManagerAgent-%version%.macos-x64.pkg
+SET pkgStagingPatharm64=~/Documents/LithnetAccessManagerAgent-%version%.macos-arm64.pkg
 SET macOSPackageFolder=/Volumes/ams
-SET macOSBuildFolder=%macOSPackageFolder%/macos-x64
+SET macOSBuildFolderx64=%macOSPackageFolder%/macos-x64
+SET macOSBuildFolderarm64=%macOSPackageFolder%/macos-arm64
 SET sshPath=C:\Program Files\Git\usr\bin
 
 if NOT "%MacOSNotarizationAppleIDPasswordEncrypted%"=="" FOR /F "tokens=*" %%g IN ('powershell -File "%buildToolsPath%\dpapi-protection.ps1" -data "%MacOSNotarizationAppleIDPasswordEncrypted%"') do (SET MacOSNotarizationAppleIDPassword=%%g)  
@@ -23,31 +25,64 @@ powershell -Command "& Remove-Item '%amsBuildFolder%\*.pkg' -recurse -force" || 
 if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
 
 ECHO [92mBuilding 64-bit intel macOS package[0m
-dotnet publish "%agentProjectMacOs%" --runtime osx-x64 --output "%outputdirx64MacOs%" --framework netcoreapp3.1 --self-contained true /property:Version=%version% /property:FileVersion=%version% 
+dotnet publish "%agentProjectMacOs%" --runtime osx-x64 --output "%outputdirx64MacOs%" --self-contained true /property:Version=%version% /property:FileVersion=%version% 
+
 REM /p:TreatWarningsAsErrors=true /warnaserror
 if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
  
 mkdir "%outputdirx64MacOs%\LithnetAccessManagerAgent"
 move "%outputdirx64MacOs%\*" "%outputdirx64MacOs%\LithnetAccessManagerAgent"
 
-ECHO [92mSigning files[0m
+ECHO [92mSigning x64 files[0m
 
-"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "codesign --sign ""%MacOSDeveloperIDApplicationCertificate%"" --keychain %MacOSSigningCertificateKeychain% --verbose --options runtime --entitlements ""%macOSBuildFolder%/LithnetAccessManagerAgent/io.lithnet.accessmanager.agent.entitlements"" --timestamp %macOSBuildFolder%/LithnetAccessManagerAgent/*.dylib"
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "codesign --sign ""%MacOSDeveloperIDApplicationCertificate%"" --keychain %MacOSSigningCertificateKeychain% --verbose --options runtime --entitlements ""%macOSBuildFolderx64%/LithnetAccessManagerAgent/io.lithnet.accessmanager.agent.entitlements"" --timestamp %macOSBuildFolderx64%/LithnetAccessManagerAgent/*.dylib"
 if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
  
-"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "codesign --sign ""%MacOSDeveloperIDApplicationCertificate%"" --keychain %MacOSSigningCertificateKeychain% --verbose --options runtime --entitlements ""%macOSBuildFolder%/LithnetAccessManagerAgent/io.lithnet.accessmanager.agent.entitlements"" --timestamp %macOSBuildFolder%/LithnetAccessManagerAgent/Lithnet.AccessManager.Agent"
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "codesign --sign ""%MacOSDeveloperIDApplicationCertificate%"" --keychain %MacOSSigningCertificateKeychain% --verbose --options runtime --entitlements ""%macOSBuildFolderx64%/LithnetAccessManagerAgent/io.lithnet.accessmanager.agent.entitlements"" --timestamp %macOSBuildFolderx64%/LithnetAccessManagerAgent/Lithnet.AccessManager.Agent"
 if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
  
-"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "pkgbuild --identifier io.lithnet.accessmanager.agent --install-location /Applications --scripts ""%macOSBuildFolder%/scripts"" --keychain %MacOSSigningCertificateKeychain% --sign ""%MacOSDeveloperIDInstallerCertificate%"" --timestamp --root %macOSBuildFolder%/ %pkgStagingPath%"
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "pkgbuild --identifier io.lithnet.accessmanager.agent --install-location /Applications --scripts ""%macOSBuildFolderx64%/scripts"" --keychain %MacOSSigningCertificateKeychain% --sign ""%MacOSDeveloperIDInstallerCertificate%"" --timestamp --root %macOSBuildFolderx64%/ %pkgStagingPathx64%"
 if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
  
-"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "xcrun notarytool submit %pkgStagingPath% --wait --apple-id %MacOSNotarizationAppleID% --team-id %MacOSNotarizationTeamID% --password %MacOSNotarizationAppleIDPassword%"
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "xcrun notarytool submit %pkgStagingPathx64% --wait --apple-id %MacOSNotarizationAppleID% --team-id %MacOSNotarizationTeamID% --password %MacOSNotarizationAppleIDPassword%"
 if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
  
-"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "xcrun stapler staple %pkgStagingPath%"
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "xcrun stapler staple %pkgStagingPathx64%"
 if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
 
-"%sshPath%\scp.exe" -r "%MacOSBuildHostUsername%@%MacOSBuildHost%:%pkgStagingPath%" "%amsBuildFolder%
+"%sshPath%\scp.exe" -r "%MacOSBuildHostUsername%@%MacOSBuildHost%:%pkgStagingPathx64%" "%amsBuildFolder%
 if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
  
+
+EXIT /b 0
+
+ECHO [92mBuilding 64-bit arm macOS package[0m
+dotnet publish "%agentProjectMacOs%" --runtime osx-arm64 --output "%outputdirarm64MacOs%" --framework net6.0 --self-contained true /property:Version=%version% /property:FileVersion=%version% /p:PublishSingleFile=true /p:DebugType=embedded
+
+if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
+ 
+mkdir "%outputdirarm64MacOs%\LithnetAccessManagerAgent"
+move "%outputdirarm64MacOs%\*" "%outputdirarm64MacOs%\LithnetAccessManagerAgent"
+
+
+ECHO [92mSigning arm64 files[0m
+
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "codesign --sign ""%MacOSDeveloperIDApplicationCertificate%"" --keychain %MacOSSigningCertificateKeychain% --verbose --options runtime --entitlements ""%macOSBuildFolderarm64%/LithnetAccessManagerAgent/io.lithnet.accessmanager.agent.entitlements"" --timestamp %macOSBuildFolderarm64%/LithnetAccessManagerAgent/*.dylib"
+if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
+ 
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "codesign --sign ""%MacOSDeveloperIDApplicationCertificate%"" --keychain %MacOSSigningCertificateKeychain% --verbose --options runtime --entitlements ""%macOSBuildFolderarm64%/LithnetAccessManagerAgent/io.lithnet.accessmanager.agent.entitlements"" --timestamp %macOSBuildFolderarm64%/LithnetAccessManagerAgent/Lithnet.AccessManager.Agent"
+if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
+ 
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "pkgbuild --identifier io.lithnet.accessmanager.agent --install-location /Applications --scripts ""%macOSBuildFolderarm64%/scripts"" --keychain %MacOSSigningCertificateKeychain% --sign ""%MacOSDeveloperIDInstallerCertificate%"" --timestamp --root %macOSBuildFolderarm64%/ %pkgStagingPatharm64%"
+if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
+ 
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "xcrun notarytool submit %pkgStagingPatharm64% --wait --apple-id %MacOSNotarizationAppleID% --team-id %MacOSNotarizationTeamID% --password %MacOSNotarizationAppleIDPassword%"
+if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
+ 
+"%sshPath%\ssh.exe" %MacOSBuildHostUsername%@%MacOSBuildHost% "xcrun stapler staple %pkgStagingPatharm64%"
+if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
+
+"%sshPath%\scp.exe" -r "%MacOSBuildHostUsername%@%MacOSBuildHost%:%pkgStagingPatharm64%" "%amsBuildFolder%
+if %errorlevel% neq 0 ECHO [91mBuild failed[0m && exit /b %errorlevel%
+
 ENDLOCAL
